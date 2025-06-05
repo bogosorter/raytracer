7da985c75ad1cec19da23bcd88f0f51a63a2b45f
bogosorter <luiswbarbosa@gmail.com>
2025-06-05 22:15:34 +0100

    added hitboxes

--- geometry.c
+++ geometry.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include "geometry.h"
 #include "settings.h"
 
@@ -30,6 +31,40 @@ double shoot_plane(ray_t ray, plane_t plane) {
     return a / b;
 }
 
+bool shoot_orthogonal_box(ray_t ray, vector_t start, vector_t end) {
+    double tmin = (start.x - ray.origin.x) / ray.direction.x;
+    double tmax = (end.x - ray.origin.x) / ray.direction.x;
+    if (tmin > tmax) {
+        double temp = tmin;
+        tmin = tmax;
+        tmax = temp;
+    }
+    if (tmax < 0) return false;
+
+    double tymin = (start.y - ray.origin.y) / ray.direction.y;
+    double tymax = (end.y - ray.origin.y) / ray.direction.y;
+    if (tymin > tymax) {
+        double temp = tymin;
+        tymin = tymax;
+        tymax = temp;
+    }
+    if (tymin > tmin) tmin = tymin;
+    if (tymax < tmax) tmax = tymax;
+    if (tmin > tmax || tmax < 0) return false;
+
+    double tzmin = (start.z - ray.origin.z) / ray.direction.z;
+    double tzmax = (end.z - ray.origin.z) / ray.direction.z;
+    if (tzmin > tzmax) {
+        double temp = tzmin;
+        tzmin = tzmax;
+        tzmax = temp;
+    }
+    if (tzmin > tmin) tmin = tzmin;
+    if (tzmax < tmax) tmax = tzmax;
+    if (tmin > tmax || tmax < 0) return false;
+    return true;
+}
+
 struct triangle_cache {
     triangle_t triangle;
     vector_t ab; // Edge vector AB
@@ -39,6 +74,9 @@ struct triangle_cache {
     vector_t barycentric_b; // Barycentric coordinate vector for B
     vector_t normal; // Normal vector of the triangle
     plane_t plane; // Plane of the triangle
+
+    vector_t hitbox_start; // Start point of the hitbox
+    vector_t hitbox_end; // End point of the hitbox
 };
 
 triangle_cache_t *triangle_cache_create(const triangle_t triangle) {
@@ -56,6 +94,14 @@ triangle_cache_t *triangle_cache_create(const triangle_t triangle) {
     
     cache->plane.point = triangle.a;
     cache->plane.normal = cache->normal;
+
+    cache->hitbox_start.x = fmin(fmin(triangle.a.x, triangle.b.x), triangle.c.x);
+    cache->hitbox_start.y = fmin(fmin(triangle.a.y, triangle.b.y), triangle.c.y);
+    cache->hitbox_start.z = fmin(fmin(triangle.a.z, triangle.b.z), triangle.c.z);
+
+    cache->hitbox_end.x = fmax(fmax(triangle.a.x, triangle.b.x), triangle.c.x);
+    cache->hitbox_end.y = fmax(fmax(triangle.a.y, triangle.b.y), triangle.c.y);
+    cache->hitbox_end.z = fmax(fmax(triangle.a.z, triangle.b.z), triangle.c.z);
 
     return cache;
 }
