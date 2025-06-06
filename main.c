a368237de0c68fa747dc8129c08c4e4db4b46f68
bogosorter <luiswbarbosa@gmail.com>
2025-06-06 21:23:04 +0100

    added windmill object

--- main.c
+++ main.c
@@ -8,28 +8,28 @@
 
 int main() {
     raytracer_settings_t settings = {
-        .height = 450,
-        .width = 800,
+        .height = 1080,
+        .width = 1920,
         .fov_angle = 1.570796327, // pi / 2
         .ambient_luminosity = 0.2,
         .light = vector_normalize((vector_t){-1, 1, 1}),
-        .camera = {-3, 0.5, 2.5},
+        .camera = {-11, 8, 10},
         .forward = vector_normalize((vector_t){1.3, -0.3, -1}),
         .up = vector_normalize((vector_t){0, 1, 0})
     };
 
-    object_t *taxi = object_load("assets/Taxi.obj");
-    if (taxi == NULL) {
+    object_t *windmill = object_load("assets/TowerWindmill.obj");
+    if (windmill == NULL) {
         printf("Error loading OBJ file\n");
         return -1;
     }
 
     uint8_t pixels[settings.height * settings.width * 3];
-    raytracer_scene(settings, taxi, pixels);
+    raytracer_scene(settings, windmill, pixels);
 
     if (bmp_export("output.bmp", settings.height, settings.width, pixels) != 0) {
         printf("Error exporting BMP file\n");
     }
 
-    object_free(taxi);
+    object_free(windmill);
 }
